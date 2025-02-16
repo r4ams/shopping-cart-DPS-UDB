5668dfaeef05c10caca2d24b42ec1c4544bb83af
r4ams <rhramos24@gmail.com>
2025-02-16 17:59:12 -0600

    Create Product class

---
+++ resources/js/product.js
@@ -0,0 +1,11 @@
+class Product {
+  constructor(id, name, price, image, stock) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.image = image;
+    this.stock = stock;
+  }
+}
+
+export default Product;
