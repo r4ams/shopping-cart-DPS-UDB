9c3d63f3182c7639784d9c40b9e726a156923b56
r4ams <rhramos24@gmail.com>
2025-02-16 22:16:26 -0600

    Set product information

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,18 @@
 import Product from "./product.js";
 import Inventory from "./inventory.js";
+import ShoppingCart from "./shoppingCart.js";
 import StoreUI from "./storeUI.js";
 
 // Datos iniciales
 const initialProducts = [
-  new Product(1, "Laptop", 1200, "https://placehold.co/200x200", 5),
-  new Product(2, "Smartphone", 800, "https://placehold.co/200x200", 10),
-  new Product(3, "Auriculares", 150, "https://placehold.co/200x200", 8),
-  new Product(4, "Memoria USB", 150, "https://placehold.co/200x200", 8)
+  new Product(1, "Daflon 500 Mg", "1 caja , 60 comprimidos recubiertos", 12.92, "./resources/images/products/product1.png", 5, 'featured'),
+  new Product(2, "Ozempic Dual Dose 2 Mg", "1 caja , 1 pluma precargada con 6 agujas", 17.00, "./resources/images/products/product2.png", 3, 'featured'),
+  new Product(3, "Rovartal NF 10 Mg", "1 caja , 30 comprimidos", 19.00, "./resources/images/products/product3.png", 4, 'featured'),
+  new Product(4, "Micardis Plus 80 mg/12.5 mg", "1 caja , 14 comprimidos recubiertos", 21.00, "./resources/images/products/product4.png", 7, 'featured'),
+  new Product(5, "Concor AM 10 Mg", "1 caja , 30 tabletas", 7.00, "./resources/images/products/product5.png", 6, 'best'),
+  new Product(6, "Daflon 1000 Mg", "1 caja , 36 comprimidos", 19.99, "./resources/images/products/product6.png", 10, 'best'),
+  new Product(7, "Micardis Plus 80 mg/12.5 mg", "1 caja , 14 comprimidos recubiertos", 16.00, "./resources/images/products/product7.png", 2, 'best'),
+  new Product(8, "Kolestevan 20 Mg", "1 caja , 30 tabletas", 29.00, "./resources/images/products/product8.png", 19, 'best'),
 ];
 
 // Inicialización del inventario
@@ -17,8 +22,11 @@ if (!inventory.loadInventory()) {
   inventory.saveInventory();
 }
 
+// Inicialización del carrito
+const cart = new ShoppingCart(inventory);
+
 // Inicialización de la UI
-const storeUI = new StoreUI(inventory);
+const storeUI = new StoreUI(inventory, cart);
 
 // Obtener los elementos
 const toggleBtn = document.getElementById('toggle-btn');
