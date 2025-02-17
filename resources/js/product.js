efb6c5c3c442d52864d41e2b0455df319d7362ce
r4ams <rhramos24@gmail.com>
2025-02-16 22:13:05 -0600

    Add new properties to Product class

--- resources/js/product.js
+++ resources/js/product.js
@@ -1,10 +1,12 @@
 class Product {
-  constructor(id, name, price, image, stock) {
+  constructor(id, name, description, price, image, stock, category) {
     this.id = id;
     this.name = name;
+    this.description = description;
     this.price = price;
     this.image = image;
     this.stock = stock;
+    this.category = category;
   }
 }
 
